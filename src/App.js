a43153e236c4a9970609856a3328c651ea118b22
BARATHLIN <112534630+Barathlin@users.noreply.github.com>
2024-11-11 23:32:27 +0530

    Tailwind installation

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="bg-blue-500 text-white p-4 rounded-lg">
+      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+      <p>This is a React component styled with Tailwind.</p>
+    </div>
       </header>
     </div>
   );
