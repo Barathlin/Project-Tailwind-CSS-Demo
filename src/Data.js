4bb67a8f887144b8ac52c5eb5a9dcd0b402e715b
BARATHLIN <112534630+Barathlin@users.noreply.github.com>
2024-11-12 16:45:27 +0530

    cardview
    card view done

--- src/Data.js
+++ src/Data.js
@@ -2,6 +2,7 @@
  import photo2 from './assets/img/360_F_878560486_3KgaJ8tyQyBMzFCzNeBvI2K2xvGyVf5l.jpg'
  import photo3 from './assets/img/young-boy-sitting-front-laptop-computer-top-wooden-table-generative-ai_97167-13661.avif'
  import photo4 from './assets/img/business-boy-working-laptop-desk-d-tiny-cute-isometric-generative-ai-business-boy-working-laptop-desk-d-tiny-288022668.webp'
+ import photo5 from './assets/img/cute-boy-using-laptop-illustration_960782-103712.avif'
  import photo6 from './assets/img/happy-company-employee-holding-phone_1106493-232626.jpg'
 
  export const data = [
